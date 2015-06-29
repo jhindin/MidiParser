6bb6c10c01219cd5830b9c86aa1eb208e7c6a994
Joseph Hindin <hindin@gmail.com>
2015-06-29 20:35:32 +0300

    FileChannel stream test - boilerplate

---
+++ MidiParser/test/com/jhindin/test/FileChannelStreamTest.java
@@ -0,0 +1,14 @@
+package com.jhindin.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileChannelStreamTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
