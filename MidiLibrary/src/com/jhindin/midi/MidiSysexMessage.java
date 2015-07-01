cc1664d23fd734a62325b3406f6ad8814a3b2508
Joseph Hindin <hindin@gmail.com>
2015-07-01 14:08:20 +0300

    Fix in toString

--- MidiLibrary/src/com/jhindin/midi/MidiSysexMessage.java
+++ MidiLibrary/src/com/jhindin/midi/MidiSysexMessage.java
@@ -6,7 +6,9 @@ public class MidiSysexMessage extends MidiVariableLengthMessage {
 	@Override
 	public String toString() {
 		StringBuilder b = new StringBuilder();
-		b.append("Sysex: status  " + Integer.toHexString(status & 0xff));
+		b.append("Sysex: status  ");
+		b.append(Integer.toHexString(status & 0xff));
+		b.append(" rest of message: ");
 		appendByteArrayAsHex(b, data, dataOffset, data.length - dataOffset);
 		return b.toString();
 	}
