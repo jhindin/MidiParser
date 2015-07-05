1ff0e7b8468b9007a558acef97314e1b532fd20c
Joseph Hindin <hindin@gmail.com>
2015-07-05 09:52:40 +0300

    Api mimicking Java library

--- MidiLibrary/src/com/jhindin/midi/Sequence.java
+++ MidiLibrary/src/com/jhindin/midi/Sequence.java
@@ -7,29 +7,45 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 public class Sequence implements Iterable<Track>{
-	short format, nTracks, division;
-	public enum DivisionMode { PPQ_DIVISION, SMTPE_DIVISION } ;
+	short format, division;
+	public enum DivisionMode { PPQ_DIVISION, SMTPE_DIVISION };
 	DivisionMode divisionMode;
 
+	public static final float PPQ          = 0.0f;
+	public static final float SMPTE_24     = 24.0f;
+	public static final float SMPTE_25     = 25.0f;
+	public static final float SMPTE_30     = 30.0f;
+    public static final float SMPTE_30DROP = 29.97f;
+    
 	short ticksPerPPQ; // for PPQ division
 	int ticksPerFrame, fps; // for SMTPE division;
 	
 	Track tracks[];
 	
+	public Sequence() {
+		format = 0;
+		division = 0;
+		divisionMode = DivisionMode.PPQ_DIVISION;
+		ticksPerPPQ = 0;
+		ticksPerFrame = 0;
+		fps = 0;
+	}
+	
 	public Sequence(InputStream is) throws IOException, MidiException {
 		if (!is.markSupported()) 
 			is = new BufferedInputStream(is);
 		
-		parseHeader(is);
+		ParsingContext context = new ParsingContext();
+		parseHeader(is, context);
 
-		tracks = new Track[nTracks];
+		tracks = new Track[context.nTracks];
 
-		for (int i = 0; i < nTracks; i++) {
+		for (int i = 0; i < context.nTracks; i++) {
 			tracks[i] = new Track(i, TrackStreamChunk.getChunk(is));
 		}
 	}
 	
-	void parseHeader(InputStream is) throws IOException, MidiException {
+	void parseHeader(InputStream is, ParsingContext context) throws IOException, MidiException {
 		MemoryChunk header = MemoryChunk.getChunk(is);
 		
 		if (header.body.length != 6) {
@@ -41,7 +57,7 @@ public class Sequence implements Iterable<Track>{
 			throw new MidiException("Unexpected format " + format);
 		}
 		
-		nTracks = bytes2Short(header.body, 2);
+		context.nTracks = bytes2Short(header.body, 2);
 		
 		division = bytes2Short(header.body, 4);
 		if ((division & 0x8000) == 0) {
@@ -61,9 +77,24 @@ public class Sequence implements Iterable<Track>{
 	public DivisionMode getDivisionMode() {
 		return divisionMode;
 	}
-
-	public short getNTracks() {
-		return nTracks;
+	
+	public float getDivisionType() {
+		if (divisionMode == DivisionMode.PPQ_DIVISION) { 
+			return PPQ;
+		} else {
+			switch (fps) {
+			case 24:
+				return SMPTE_24;
+			case 25:
+				return SMPTE_25;
+			case 29:
+				return SMPTE_30DROP;
+			case 30:
+				return SMPTE_30;
+			default:
+				return -1.0f;
+			}
+		}
 	}
 
 	public short getTicksPerPPQ() {
@@ -85,6 +116,15 @@ public class Sequence implements Iterable<Track>{
 	@Override
 	public Iterator<Track> iterator() {
 		return Arrays.asList(tracks).iterator();
+	}
+	
+	public Track[] getTracks() {
+		return tracks;
+	}
+	
+	
+	protected class ParsingContext {
+		int nTracks = 0;
 	}
 
 }
